4a9e1f1e1caca42472c772798d64f3f7fdb9dcfb
edwardalx <eobkwaku@gmail.com>
2025-07-09 23:22:55 +0100

    Ex 7

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,5 +3,15 @@ let b = ['fun','cool','amazing']
 
 
 for(x=0;x<3; x++){
-    console.log(a[x] + " is " + b[x])
+    console.log(a[x] + " is " + b[x]);
 }
+
+let i = 0
+while (i<a.length) {
+    console.log(a[i] + " is " + b[i]);
+    i++
+}
+
+a.forEach((element,position) => {
+    console.log(element + " is " + b[position]);
+});
