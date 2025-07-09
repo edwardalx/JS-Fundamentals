9685022de387b462fbffa5f462418adb1b00d2dc
edwardalx <eobkwaku@gmail.com>
2025-07-09 21:32:49 +0100

    Ex 4

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,19 @@
 function myfunc(arg){
-   console.log(arg)
-}
+     if (arg[0] === undefined){
+        console.log("No argument");
+        }
+     else{
+       arg.forEach(element => {
+        console.log(element)
+       });
+    }
+} 
 myfunc(process.argv.slice(2))
+
+// const args = process.argv.slice(2);
+
+// if (args[0] === undefined) {
+//     console.log("No argument");
+// } else {
+//     console.log(args[0]);
+// }
