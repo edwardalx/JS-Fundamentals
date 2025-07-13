70827133066d883561de024f7fd9572ba068c87c
edwardalx <eobkwaku@gmail.com>
2025-07-13 22:37:13 +0100

    Ex 10

--- 9-add.js
+++ 9-add.js
@@ -1,13 +1,14 @@
 
 function myAddFunc(arg){
-    let num1 = arg[0];
-    let num2 = arg[1];
-    if (num2 === undefined){
-        {console.log("NaN")}
+    let num1 = Number(arg[0]);
+    let num2 = Number(arg[1]);
+    if (isNaN(num1)||isNaN(num2)){
+        console.log("NaN")
+        return
     }
-    else if (num1 === undefined){console.log("NaN")}
+    // if (num1 === undefined){console.log("NaN")}
 
-    return console.log(Number(num1) + Number(num2))
+    return console.log(num1 + num2)
 
 }
 myAddFunc(process.argv.slice(2))
